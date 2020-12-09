ae44b86bfdcda28b98146f3540d2226403bd04a0
thaliadettenborn <thalia.born@gmail.com>
2020-12-09 14:39:37 -0300

    adding user info in req

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const sessionsRepository = require("../models/sessions");
+const UserRepository = require("../models/users");
 
 async function authMiddleware(req,res,next){
   const auth = req.header('Authorization');
@@ -10,8 +11,11 @@ async function authMiddleware(req,res,next){
   const session = await sessionsRepository.findByToken(tokenHeader);
   if(!session) return res.status(401).send({ message: 'Invalid token' });
 
+  const user = await UserRepository.findById(userId);
+  if(!session) return res.status(404).send({ message: 'User not found' });
+  
   req.userId = session.userId;
-  req.session = session;
+  req.user = { userId: user.id, cpf: user.cpf };
   next();
 }
 
