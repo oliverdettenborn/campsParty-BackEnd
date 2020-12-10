23c86ff87453c6c4d8cfc3aa3b3cf2b012a56a4a
monalice <alice.amorim@engenharia.ufjf.br>
2020-12-10 20:07:31 -0300

    merge

--- src/schemas/userSchemas.js
+++ src/schemas/userSchemas.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const signUp = Joi.object({
     cpf: Joi.string().pattern(/^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$/, 'cpf inválido').required(),
     email: Joi.string().email().required(),
+    ticketType: Joi.string().valid('none','hotel','tent').required(),
     password: Joi.string().alphanum().min(6).max(16).required(),
     passwordConfirmation: Joi.ref('password')
 });
