ebe917b8b995374f9148f95178fafdb55e0f5187
thaliadettenborn <thalia.born@gmail.com>
2020-12-11 15:14:22 -0300

    adjust validation form put subscription

--- src/schemas/subscription.js
+++ src/schemas/subscription.js
@@ -10,9 +10,12 @@ const subspriptionSchema = Joi.object({
   uf: Joi.string().length(2).required(),
   postalCode: Joi.string().length(9).pattern(/^\d{5}-\d{3}$/, 'validate CEP to 99999-99').required(),
   gender: Joi.string().valid('M','F','NI').required(),
-  accommodationId: Joi.string().pattern(/^[\d]+$/, "only numbers").required(),
+  accommodationId: Joi.number().integer().required(),
   phone: Joi.string().pattern(/^(\(\d{2}\)\s)(\d{4,5}\-\d{4})$/, 'validate phone to (99) 99999-9999').required(),
-  admissionCost: Joi.string().required()
+  admissionCost: Joi.string().required(),
+  id: Joi.number().integer(),
+  userId: Joi.number().integer(),
+  cpf: Joi.string(),
 })
 
 module.exports = subspriptionSchema;
